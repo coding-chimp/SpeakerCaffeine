a75bda6658cc70e2dcadfb6621f28035ddaa029e
coding-chimp <xarfai27@gmail.com>
2017-04-10 19:59:10 +0200

    Initial Commit

---
+++ SpeakerCaffeine/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  SpeakerCaffeine
+//
+//  Created by Bastian Bartmann on 10.04.17.
+//  Copyright © 2017 Bastian Bartmann. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+    // Do any additional setup after loading the view.
+  }
+
+  override var representedObject: Any? {
+    didSet {
+    // Update the view, if already loaded.
+    }
+  }
+
+
+}
+
